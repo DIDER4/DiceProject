18412bf5344796a50b61fb22e038a0d81c0bec35
BuildTools <unconfigured@null.spigotmc.org>
2024-09-18 16:24:15 +0200

    initial commit for this project

--- src/opgave02/Craps.java
+++ src/opgave02/Craps.java
@@ -1,9 +1,74 @@
 package opgave02;
 
+import java.util.Scanner;
+
 public class Craps {
+    private static int rollCount = 0;
+    private static int sum = 0;
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("Test2");
+        System.out.println("Velkommen til spillet, Craps.");
+        printRules();
+        System.out.println();
+
+        playDice();
+
+        System.out.println();
+        System.out.println("Tak for at spille, Craps.");
     }
 
+    private static void printRules() {
+        System.out.println("=====================================================");
+        System.out.println("Regler for Craps");
+        System.out.println("Spillet består af en række kast med to terninger. Udfaldet af et kast er summen af de to terningers\n" +
+                "øjne.\n" +
+                "Det første kast kaldes ‘come out roll’. Spilleren vinder med det samme, hvis det første kast er 7\n" +
+                "eller 11, og taber med det samme, hvis det første kast er 2, 3 eller 12. Hvis spillerens første kast er\n" +
+                "4, 5, 6, 8, 9 eller 10, etableres dette tal som spillerens ‘point’. Spilleren bliver derefter ved med at\n" +
+                "kaste, indtil han enten kaster sit ‘point’ igen eller kaster 7. Kaster han 7, har han tabt. Kaster han\n" +
+                "sit ’point’, har han vundet");
+        System.out.println("=====================================================");
+    }
+
+    public static void playDice() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Klar til at prøve lykken med de 2 terninger? ('ja/nej') ");
+        String answer = scanner.nextLine();
+        while (!answer.equals("nej")) {
+            int[] faces = rollDice();
+            System.out.println("Du rullede: " + faces[0]);
+            System.out.println("Du rullede: " + faces[1]);
+            sum += faces[0] + faces[1];
+
+            updateStatistics();
+
+            System.out.print("Rul en terning? ('ja/nej') ");
+            answer = scanner.nextLine();
+        }
+        printStatistics();
+        scanner.close();
+    }
+
+    private static int[] rollDice() {
+        int[] dices = new int[2];
+        dices[0] = (int) (Math.random() * 6 + 1);
+        dices[1] = (int) (Math.random() * 6 + 1);
+        return dices;
+    }
+
+    private static int rollDie() {
+        return (int) (Math.random() * 6 + 1);
+    }
+
+    private static void updateStatistics() {
+        rollCount++;
+    }
+
+    private static void printStatistics() {
+        System.out.println("\nResults:");
+        System.out.println("-------");
+        System.out.printf("%17s %4d\n", "Antal rul:", rollCount);
+
+    }
 }
